3a6d012fef3c17ce16c428a3311a9469d4baa365
Nitish-HR <nitishramashetty@gmail.com>
2026-04-10 12:54:53 +0530

    fixing env

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -6,13 +6,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAZhk6ONlrLppdGMf7nWHbWGAXkPHSVqi0",
-  authDomain: "pulseloop-f5b68.firebaseapp.com",
-  projectId: "pulseloop-f5b68",
-  storageBucket: "pulseloop-f5b68.firebasestorage.app",
-  messagingSenderId: "464675985521",
-  appId: "1:464675985521:web:c2eaf835698fb65acfe3d0"
+const firebaseConfig = { 
+  apiKey: process.env.apiKey_firebase,
+  authDomain: process.env.authDomain__firebase,
+  projectId: process.env.projectId_firebase,
+  storageBucket: process.env.storageBucket_firebase,
+  messagingSenderId: process.env.messagingSenderId_firebase,
+  appId: process.env.appId_firebase
 };
 
 // Initialize Firebase
